f2213d81af8c398507db782ef03e60d9dd4ef1db
Dwayne LaForce <dwayne.laforce@gmail.com>
2020-12-21 16:06:34 -0800

    just about finished with title screen

--- AdventureGame/src/Game.java
+++ AdventureGame/src/Game.java
@@ -2,6 +2,7 @@ import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -10,54 +11,70 @@ public class Game {
 	public static void main(String[] args) {
 		new Game();
 	}
+	
 	// game attributes
 	JFrame window;
 	Container container;
 	
-	Font titleFont = new Font("Papyrus", Font.PLAIN, 100);
+	Font titleFont = new Font("Papyrus", Font.PLAIN, 92);
 	JPanel titlePanel;
 	JLabel titleLabel;
 	
-	Font buttonFont = new Font("Papyrus", Font.ITALIC, 40);
+	Font subtitleFont = new Font("Papyrus", Font.ITALIC, 18);
+	JPanel subtitlePanel;
+	JLabel subtitleLabel;
+	
+	Font buttonFont = new Font("Papyrus", Font.ITALIC, 32);
 	JPanel startButtonPanel;
 	JLabel startButtonLabel;
-	
-	
-	
+	JButton startButton;
 	
 	// constructor
 	public Game() {
 		window = new JFrame();
-			window.setSize(1600, 900);
+			window.setSize(1000, 600);
 			window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			window.getContentPane().setBackground(Color.darkGray);
-			window.setLayout(null);
+			window.setLayout(null); // not sure what this does but hey it works
 			window.setVisible(true);
 		
 		container = window.getContentPane();
 		
 		titlePanel = new JPanel();
-			titlePanel.setBounds(400, 200, 800, 200);
+			titlePanel.setBounds(100, 100, 800, 150);
 			titlePanel.setBackground(Color.darkGray);
-			
 			titleLabel = new JLabel("Adventure Game");
 				titleLabel.setForeground(Color.GRAY);
 				titleLabel.setFont(titleFont);
-		
 			titlePanel.add(titleLabel);
 		
-		startButtonPanel = new JPanel();
-			startButtonPanel.setBounds(400, 600, 800, 200);
-			startButtonPanel.setBackground(Color.DARK_GRAY);
+		subtitlePanel = new JPanel();
+			subtitlePanel.setBounds(100, 250, 800, 50);
+			subtitlePanel.setBackground(Color.darkGray);
+			subtitleLabel = new JLabel("by Anindo Khan & Dwayne LaForce | © 2020");
+				subtitleLabel.setForeground(Color.lightGray);
+				subtitleLabel.setFont(subtitleFont);
+			subtitlePanel.add(subtitleLabel);
+		
 			
-			startButtonLabel = new JLabel("...click to embark on your journey...");
+			
+		startButtonPanel = new JPanel();
+			startButtonPanel.setBounds(300, 350, 400, 200);
+			startButtonPanel.setBackground(Color.darkGray);
+			startButtonLabel = new JLabel("... embark on your journey ...");
 				startButtonLabel.setForeground(Color.red);
 				startButtonLabel.setFont(buttonFont);
 			
+			startButton = new JButton("start");
+				startButton.setFont(buttonFont);
+				startButton.setBackground(Color.black);
+				startButton.setForeground(Color.lightGray);
 			startButtonPanel.add(startButtonLabel);
-		
+			startButtonPanel.add(startButton);
 		
 		container.add(titlePanel);
+		container.add(subtitlePanel);
+		
 		container.add(startButtonPanel);
 		
 	}
