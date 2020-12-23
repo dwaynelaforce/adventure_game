b366847a0fbce23cfada7846be80eb8a7a2a5f0a
Dwayne LaForce <dwayne.laforce@gmail.com>
2020-12-22 20:40:45 -0800

    starting the story

--- AdventureGame/src/Home.java
+++ AdventureGame/src/Home.java
@@ -12,25 +12,22 @@ public class Home extends Room {
 		Choice3Listener c3l = new Choice3Listener();
 		Choice4Listener c4l = new Choice4Listener();
 		this.textArea.setText(String.format(
-			"You open your eyes. Slowly, as the direct sunlight is a bit blinding. "
-			+ "The outside world is a complete blur at first, but after a few "
-			+ "seconds your sight sharpens. It becomes apparent you are in the back "
-			+ "of a horse-drawn wagon, driven by a soldier in imperial garb. "
-			+ "Your hands are tied with rope. You're not alone- there are three "
-			+ "others in the cart- hands bound just like yours, but one is dressed in "
-			+ "fine clothing and also gagged with a piece of cloth."
-			+ "%n"
-			+ "\"Hey, you. You’re finally awake. You were trying to cross the border, "
-			+ "right?  Walked right into that Imperial ambush, same as us, and that "
-			+ "thief over there...\" "
+			"As you walk along the forest road, you come upon a "
+			+ "small, wooden bridge spanning a deep ravine. "
+			+ "Between you and the bridge is a big ugly troll. %n"
+			+ "He says \"My toll to cross this here bridge is 5 copper pieces. "
+			+ "Man, I\'m hungry though.  I would waive my fee if you had something "
+			+ "to eat...\" %n"
+			+ "You have no money, but earlier you remember seeing a grove of apple "
+			+ "tress back up the road."
 			));
-		this.choice1.setText("Wizard");
+		this.choice1.setText("go to apple tree grove");
 		this.choice1.addActionListener(c1l);
-		this.choice2.setText("Dragon");
+		this.choice2.setText("try to shove through");
 		this.choice2.addActionListener(c2l);
-		this.choice3.setText("Corgi");
+		this.choice3.setText("give apple");
 		this.choice3.addActionListener(c3l);
-		this.choice4.setText("human");
+		this.choice4.setText("stab");
 		this.choice4.addActionListener(c4l);
 		this.pushPanelsToGUI();
 	}
